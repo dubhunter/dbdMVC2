f8bf83e83a582023d8e2782b67e912e4b1aeef85
dubhunter <get@willmason.me>
2011-01-26 21:27:45 +0000

    put old API endpoints back
    git-svn-id: file:///var/svn/dbdMVC2/trunk@49 78647451-d9dc-4e60-9644-f91bfbd15ed2

--- plugins/reCaptcha.php
+++ plugins/reCaptcha.php
@@ -2,9 +2,9 @@
 /**
  * The reCAPTCHA server URL's
  */
-define("RECAPTCHA_API_SERVER", "http://www.google.com/recaptcha/api");
-define("RECAPTCHA_API_SECURE_SERVER", "https://www.google.com/recaptcha/api");
-define("RECAPTCHA_VERIFY_SERVER", "www.google.com");
+define("RECAPTCHA_API_SERVER", "http://api.recaptcha.net");
+define("RECAPTCHA_API_SECURE_SERVER", "https://api-secure.recaptcha.net");
+define("RECAPTCHA_VERIFY_SERVER", "api-verify.recaptcha.net");
 
 class reCaptcha
 {
