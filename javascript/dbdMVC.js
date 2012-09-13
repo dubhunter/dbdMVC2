1f3fb3cf67f53aab880ccc748c072a779a5c2249
dubhunter <get@willmason.me>
2012-09-13 05:16:19 +0000

    spaces to tabs
    git-svn-id: file:///var/svn/dbdMVC2/trunk@90 78647451-d9dc-4e60-9644-f91bfbd15ed2

--- javascript/dbdMVC.js
+++ javascript/dbdMVC.js
@@ -4,63 +4,63 @@
  */
 // Inspired by base2 and Prototype and Modifed by Will Mason (2011-04-03)
 (function(){
-  var constructing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\bparent\b/ : /.*/;
+	var constructing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\bparent\b/ : /.*/;
 
-  // The base Class implementation (does nothing)
-  this.Class = function(){};
+	// The base Class implementation (does nothing)
+	this.Class = function(){};
 
-  // Create a new Class that inherits from this class
-  Class.extend = function(prop) {
-    var parent = this.prototype;
+	// Create a new Class that inherits from this class
+	Class.extend = function(prop) {
+		var parent = this.prototype;
 
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    constructing = true;
-    var prototype = new this();
-    constructing = false;
+		// Instantiate a base class (but only create the instance,
+		// don't run the init constructor)
+		constructing = true;
+		var prototype = new this();
+		constructing = false;
 
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" &&
-        typeof parent[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this.parent;
+		// Copy the properties over onto the new prototype
+		for (var name in prop) {
+			// Check if we're overwriting an existing function
+			prototype[name] = typeof prop[name] == "function" &&
+				typeof parent[name] == "function" && fnTest.test(prop[name]) ?
+				(function(name, fn){
+					return function() {
+						var tmp = this.parent;
 
-            // Add a new ._parent() method that is the same method
-            // but on the parent-class
-            this.parent = parent[name];
+						// Add a new ._parent() method that is the same method
+						// but on the parent-class
+						this.parent = parent[name];
 
-            // The method only need to be bound temporarily, so we
-            // destroy it when we're done executing
-            var ret = fn.apply(this, arguments);
-            this.parent = tmp;
+						// The method only need to be bound temporarily, so we
+						// destroy it when we're done executing
+						var ret = fn.apply(this, arguments);
+						this.parent = tmp;
 
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
-    }
+						return ret;
+					};
+				})(name, prop[name]) :
+				prop[name];
+		}
 
-    // The dummy class constructor
-    function Class() {
-      // All construction is actually done in the __construct method
-      if ( !constructing && this.__construct )
-        this.__construct.apply(this, arguments);
-    }
+		// The dummy class constructor
+		function Class() {
+			// All construction is actually done in the __construct method
+			if ( !constructing && this.__construct )
+				this.__construct.apply(this, arguments);
+		}
 
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
+		// Populate our constructed prototype object
+		Class.prototype = prototype;
 
-    // Enforce the constructor to be what we expect
-    Class.constructor = Class;
+		// Enforce the constructor to be what we expect
+		Class.constructor = Class;
 
-    // And make this class extendable
-    Class.extend = arguments.callee;
+		// And make this class extendable
+		Class.extend = arguments.callee;
 
-    return Class;
-  };
+		return Class;
+	};
 })();
 
 //some string functions
